3b95280ea075b81718a4f31f44235333455eb8e1
sourtaste000 <me@sourtaste000.dev>
2021-10-31 21:51:53 -0700

    now we figure out how to run the jar

--- scripts/logs/success.sh
+++ scripts/logs/success.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -e "\033[32m$1\033[0m"
+echo -e "[\033[32m SUCCESS \033[0m] \033[32m$1\033[0m"
--- scripts/logs/warning.sh
+++ scripts/logs/warning.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -e "\033[33m$1\033[0m"
+echo -e "[\033[33m WARNING \033[0m] \033[33m$1\033[0m"
