adb1af88433eeb6aec9a476761f75025a455bbf2
sourtaste000 <me@sourtaste000.dev>
2021-11-08 11:36:20 -0800

    i thought i did everything right

--- launched.sh
+++ launched.sh
@@ -21,75 +21,75 @@ if [ ! -d "data" ]; then
   mkdir -p data
 fi
 
-if [ ! -f ".response.json.gpg" ]; then
-  if [ ! -f ".auth.json.gpg" ]; then
-    # ask for credentials
-    ./scripts/logs/info.sh "Enter email, or name for lagacy accounts:"
-    read -r username
+# if [ ! -f ".response.json.gpg" ]; then
+#   if [ ! -f ".auth.json.gpg" ]; then
+#     # ask for credentials
+#     ./scripts/logs/info.sh "Enter email, or name for lagacy accounts:"
+#     read -r username
 
-    ./scripts/logs/info.sh "Enter account password:"
-    read -r -s password
-  else
-    ./scripts/logs/info.sh "Decrypting credentials... (May prompt for password)"
-    gpg .auth.json.gpg
-    # get username
-    username=$(jq -r '.username' .auth.json)
-    # get password
-    password=$(jq -r '.password' .auth.json)
-    rm .auth.json
-    ./scripts/logs/info.sh "Authenticating..."
-  fi
-  # Authenticate and save the password and username
-  response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"agent\":{\"name\":\"Minecraft\",\"version\":1},\"username\":\"$username\",\"password\":\"$password\"}" https://authserver.mojang.com/authenticate)
-  # test if response has no content
-  if [ -z "$response" ]; then
-    ./scripts/logs/error.sh "Request to mojang auth server failed."
-    exit 1
-  fi
-  if [[ $response == *"errorMessage"* ]]; then
-    ./scripts/logs/error.sh "$(jq '.errorMessage' <<<"$response")"
-    rm .auth.json.gpg
-    exit 1
-  else
-    ./scripts/logs/success.sh "Authentication successful."
-    if [ ! -f ".auth.json.gpg" ]; then
-      echo "Saving username and password to file... (May prompt for password)"
-      echo "{\"username\":\"$username\", \"password\": \"$password\"}" > .auth.json
-      gpg -c .auth.json
-      rm .auth.json
-      ./scripts/logs/success.sh "Saved."
-    fi
-    ./scripts/logs/info.sh "Saving auth response to file... (May prompt for password)"
-    echo "$response" >.response.json
-    gpg -c .response.json
-    rm .response.json
-    ./scripts/logs/success.sh "Saved."
-  fi
-else
-  ./scripts/logs/info.sh "Decrypting credentials... (May prompt for password)"
-  gpg .response.json.gpg
-  ./scripts/logs/info.sh "Refreshing token..."
-  response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"accessToken\": \"$(jq -r '.accessToken' .response.json)\", \"clientToken\": \"$(jq -r '.clientToken' .response.json)\", \"selectedProfile\": {\"id\": \"$(jq -r '.selectedProfile | .id' .response.json)\", \"name\": \"$(jq -r '.selectedProfile | .name' .response.json)\"}}" https://authserver.mojang.com/refresh)
-  if [ -z "$response" ]; then
-    ./scripts/logs/error.sh "Request to mojang auth server failed."
-    rm .response.json
-    exit 1
-  fi
-  if [[ $response == *"errorMessage"* ]]; then
-    ./scripts/logs/error.sh "$(jq '.errorMessage' <<<"$response")"
-    curl -s -X POST -H "Content-Type: application/json" -d "{\"accessToken\": \"$(jq -r '.accessToken' .response.json)\", \"clientToken\": \"$(jq -r '.clientToken' .response.json)\"}" https://authserver.mojang.com/invalidate
-    rm .response.json
-    rm .response.json.gpg
-    exit 1
-  else
-    ./scripts/logs/success.sh "Refresh successful."
-    ./scripts/logs/info.sh "Saving response to file... (May prompt for password)"
-    echo "$response" >.response.json
-    gpg -c .response.json
-    rm .response.json
-    ./scripts/logs/success.sh "Saved."
-  fi
-fi
+#     ./scripts/logs/info.sh "Enter account password:"
+#     read -r -s password
+#   else
+#     ./scripts/logs/info.sh "Decrypting credentials... (May prompt for password)"
+#     gpg .auth.json.gpg
+#     # get username
+#     username=$(jq -r '.username' .auth.json)
+#     # get password
+#     password=$(jq -r '.password' .auth.json)
+#     rm .auth.json
+#     ./scripts/logs/info.sh "Authenticating..."
+#   fi
+#   # Authenticate and save the password and username
+#   response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"agent\":{\"name\":\"Minecraft\",\"version\":1},\"username\":\"$username\",\"password\":\"$password\"}" https://authserver.mojang.com/authenticate)
+#   # test if response has no content
+#   if [ -z "$response" ]; then
+#     ./scripts/logs/error.sh "Request to mojang auth server failed."
+#     exit 1
+#   fi
+#   if [[ $response == *"errorMessage"* ]]; then
+#     ./scripts/logs/error.sh "$(jq '.errorMessage' <<<"$response")"
+#     rm .auth.json.gpg
+#     exit 1
+#   else
+#     ./scripts/logs/success.sh "Authentication successful."
+#     if [ ! -f ".auth.json.gpg" ]; then
+#       echo "Saving username and password to file... (May prompt for password)"
+#       echo "{\"username\":\"$username\", \"password\": \"$password\"}" > .auth.json
+#       gpg -c .auth.json
+#       rm .auth.json
+#       ./scripts/logs/success.sh "Saved."
+#     fi
+#     ./scripts/logs/info.sh "Saving auth response to file... (May prompt for password)"
+#     echo "$response" >.response.json
+#     gpg -c .response.json
+#     rm .response.json
+#     ./scripts/logs/success.sh "Saved."
+#   fi
+# else
+#   ./scripts/logs/info.sh "Decrypting credentials... (May prompt for password)"
+#   gpg .response.json.gpg
+#   ./scripts/logs/info.sh "Refreshing token..."
+#   response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"accessToken\": \"$(jq -r '.accessToken' .response.json)\", \"clientToken\": \"$(jq -r '.clientToken' .response.json)\", \"selectedProfile\": {\"id\": \"$(jq -r '.selectedProfile | .id' .response.json)\", \"name\": \"$(jq -r '.selectedProfile | .name' .response.json)\"}}" https://authserver.mojang.com/refresh)
+#   if [ -z "$response" ]; then
+#     ./scripts/logs/error.sh "Request to mojang auth server failed."
+#     rm .response.json
+#     exit 1
+#   fi
+#   if [[ $response == *"errorMessage"* ]]; then
+#     ./scripts/logs/error.sh "$(jq '.errorMessage' <<<"$response")"
+#     curl -s -X POST -H "Content-Type: application/json" -d "{\"accessToken\": \"$(jq -r '.accessToken' .response.json)\", \"clientToken\": \"$(jq -r '.clientToken' .response.json)\"}" https://authserver.mojang.com/invalidate
+#     rm .response.json
+#     rm .response.json.gpg
+#     exit 1
+#   else
+#     ./scripts/logs/success.sh "Refresh successful."
+#     ./scripts/logs/info.sh "Saving response to file... (May prompt for password)"
+#     echo "$response" >.response.json
+#     gpg -c .response.json
+#     rm .response.json
+#     ./scripts/logs/success.sh "Saved."
+#   fi
+# fi
 
 if [ "$(ls -A instances)" ]; then
   ./scripts/logs/success.sh "Instances folder is not empty."
@@ -124,7 +124,7 @@ else
       exit 1
     fi
     ./scripts/logs/info.sh "Downloading $version jar..."
-    mkdir -p "./data/$name"
+    mkdir -p "./data/$version"
     mkdir -p "./instances/$name/libraries"
     if [ ! -f "./data/$version/version.json" ]; then
       curl --progress-bar "$(jq -r ".versions[] | select(.id == \"$version\") | .url" ./data/version_manifest.json <<<cat)" > "./data/$version/version.json"
@@ -132,33 +132,50 @@ else
     curl --progress-bar "$(jq -r '.downloads | .client | .url' "./data/$version/version.json" <<<cat)" -o "./instances/$name/client.jar"
     ./scripts/logs/info.sh "Downloading $version libraries..."
     # TODO: Verify sha1 which is '.libraries[] | .downloads | .artifact | .sha1' or '.libraries[] | .downloads | .classifiers | $system | .sha1'
-    jq -r '.libraries[] | .downloads | .artifact | .url' "./data/$version/version.json" <<<cat | sed -r 's/null//' | while read -r line; do
-      if [ -z "$line" ]; then
+    jq -r '.libraries[] | .downloads | .artifact | .path' "./data/$version/version.json" <<<cat | sed -r 's/null//' | while read -r downloadpath; do
+      if [ -z "$downloadpath" ]; then
         continue
       fi
-      ./scripts/logs/info.sh "Downloading $line..."
-      curl --progress-bar "$line" -o "./instances/$name/libraries/$(basename "$line")"
+      ./scripts/logs/info.sh "Downloading $downloadpath..."
+      mkdir -p "./instances/$name/libraries/$(dirname "$downloadpath")"
+      curl --progress-bar "https://libraries.minecraft.net/$downloadpath" -o "./instances/$name/libraries/$downloadpath"
     done
-    jq -r ".libraries[] | .downloads | .classifiers | .[\"$system\"] | .url" "./data/$version/version.json" <<<cat | sed -r 's/null//' | while read -r line; do
-      if [ -z "$line" ]; then
+    jq -r ".libraries[] | .downloads | .classifiers | .[\"$system\"] | .path" "./data/$version/version.json" <<<cat | sed -r 's/null//' | while read -r downloadpath; do
+      if [ -z "$downloadpath" ]; then
         continue
       fi
-      ./scripts/logs/info.sh "Downloading $line..."
-      curl --progress-bar "$line" -o "./instances/$name/libraries/$(basename "$line")"
+      ./scripts/logs/info.sh "Downloading $downloadpath..."
+      mkdir -p "./instances/$name/libraries/$(dirname "$downloadpath")"
+      curl --progress-bar "https://libraries.minecraft.net/$downloadpath" -o "./instances/$name/libraries/$downloadpath"
     done
     ./scripts/logs/success.sh "Downloaded."
     break
   done
 fi
 
-# list the files present in the instances folder and save it to an array
-files=()
-while IFS='' read -r line; do files+=("$(pwd)/instances/1point8/libraries/$line"); done < <(ls instances/1point8/libraries)
-classpath=$(IFS=: ; echo "${files[*]}") 
+
+# https://login.live.com/oauth20_authorize.srf?client_id=37af3da7-fe1a-4774-9005-88c9ee0cfd34&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A6942&scope=XboxLive.signin%20offline_access
+
+classpath=()
+classpathstring=""
+jq -r '.libraries[] | .downloads | .artifact | .path' "./data/$version/version.json" <<<cat | sed -r 's/null//' | while read -r tempclasspath; do
+  if [ -z "$tempclasspath" ]; then
+    continue
+  fi
+  classpath+=("$(pwd)/instances/$name/libraries/$tempclasspath")
+done
+unset tempclasspath
+jq -r ".libraries[] | .downloads | .classifiers | .[\"$system\"] | .path" "./data/$version/version.json" <<<cat | sed -r 's/null//' | while read -r tempclasspath; do
+  if [ -z "$tempclasspath" ]; then
+    continue
+  fi
+  classpath+=("$(pwd)/instances/$name/libraries/$tempclasspath")
+done
+classpathstring=$(IFS=: ; echo "${classpath[*]}")
 
 cd "./instances/$name" || exit 1
-java -Djava.library.path="$(pwd)/instances/$name/libraries" -cp "$classpath:$(pwd)/instances/$name/client.jar" "$(jq -r '.mainClass' "../../data/$version/version.json" <<<cat)"
-# Exception in thread "main" java.lang.UnsatisfiedLinkError: no lwjgl in java.library.path: /Users/sourtaste000/Developer/launched/instances/1point8/libraries/
+java -Djava.library.path="$(pwd)/libraries" -cp "$classpathstring:$(pwd)/client.jar" "$(jq -r '.mainClass' "../../data/$version/version.json" <<<cat)"
+# Exception in thread "main" java.lang.UnsatisfiedLinkError: no lwjgl in java.library.path: /Users/sourtaste000/Developer/launched/instances/1point8/libraries/org/lwjgl/lwjgl/lwjgl/2.9.1
 #         at java.base/java.lang.ClassLoader.loadLibrary(ClassLoader.java:2429)
 #         at java.base/java.lang.Runtime.loadLibrary0(Runtime.java:818)
 #         at java.base/java.lang.System.loadLibrary(System.java:1989)
